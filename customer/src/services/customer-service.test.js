aed6da61b3389d3114db2038b9a74c51bdf3eabb
nong.pham <nongpham.smf@gmail.com>
2024-12-05 21:55:59 +0700

    update

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,6 +1,6 @@
 // which service it is
 describe("CustomerService", () => {
-  // Which function we are testing
+  // Which function we are testing1
   describe("SignIn", () => {
     // Which Scenario we are testing
     test("validate user inputs", () => {});
